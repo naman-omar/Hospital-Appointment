d3233ec563dfd4222928e352bdd52fe513d857bb
naman-omar <namanknp13@gmail.com>
2024-07-10 20:28:21 +0530

    New change

---
+++ Frontend/src/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routers from "../routes/Routers";
+
+const Layout = () => {
+    return(
+        <div>
+            <Header/>
+            <main>
+                <Routers/>
+            </main>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ Frontend/src/routes/Routers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Routers = () => {
+    return(
+        <div>
+            Routers
+        </div>
+    );
+};
+
+export default Routers;
