dfa622eacbd59ff933383e3254324f72b5744c7e
Naman_omar <143309956+naman-omar@users.noreply.github.com>
2024-07-21 22:01:12 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -36,11 +36,11 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOption));
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/doctors", doctorRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/bookings", bookingRoute);
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/doctors", doctorRoute);
+app.use("/reviews", reviewRoute);
+app.use("/bookings", bookingRoute);
 
 app.listen(port, () => {
   connectDB();
