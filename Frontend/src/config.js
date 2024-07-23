9cba5c61d95323f793c1714c72902868b540e4b3
Naman_omar <143309956+naman-omar@users.noreply.github.com>
2024-07-23 23:56:43 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
 
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "https://hospital-appointment-backend-gukq.onrender.com"
 export const token = localStorage.getItem('token');
